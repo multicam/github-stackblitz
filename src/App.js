a9ce2f5b0b9435db5bfea4604e4c255b921736f9
multicam <jmgiorgi@gmail.com>
2019-05-17 15:44:52 +1000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,14 +9,8 @@ class App extends Component {
         <header className="App-header"><p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+<p>coucou</p>
         </header>
       </div>
     );
